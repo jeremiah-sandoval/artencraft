d4856e460476a9724386daf7db9f1b508f2e5aa7
Jeremiah Sandoval <jeremiah.sandoval@gmail.com>
2018-06-16 03:10:52 +0800

    OT

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
   function closenav(){
-    $('#main-nav').css('width','0px');
+    $('#main-nav').css({'display':'none', 'width': '0px'});
     $('#mainnav-overlay').hide();
   }
   function opennav(){
-    $('#main-nav').css('width','300px');
+    $('#main-nav').css({'display':'block', 'width': '300px'});
     $('#mainnav-overlay').show();
   }
   $('#mainnav-trigger').click(function(){ opennav(); });
   $('#mainnav-close').click(function(){ closenav(); });
+  $('#submenu-close').click(function(){ closenav(); });
   $('#mainnav-overlay').click(function(){ closenav(); });
+  $('#main-nav ul li').hover(function(){
+    $(this).find('.submenu').toggle();
+  });
+
 });
